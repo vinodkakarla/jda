13cfa3accbcaf6b1291b9bf3af9993ecbf4cfaf7
vinodk <vinod.kakarla@imaginea.com>
2016-06-06 13:51:39 +0530

    Changes to Origin

---
+++ src/main/java/com/jda/hacker/controller/DataController.java
@@ -0,0 +1,14 @@
+package com.jda.hacker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/global-admin")
+public class DataController {
+
+	@RequestMapping("/greeting")
+	void test() {
+
+	}
+}
